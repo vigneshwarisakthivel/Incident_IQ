ffcda42128377f6fd9f72f7ce8a5bc6029345680
vigneshwarisakthivel <vigneshwarisakthivel18@gmail.com>
2026-03-24 13:13:10 +0530

    coo

--- shap_backend/back/back/settings.py
+++ shap_backend/back/back/settings.py
@@ -74,10 +74,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
+   
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -108,7 +109,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://incident-iq-navy.vercel.app",  # Add your frontend URL
     # Add any other frontend domains
 ]
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
